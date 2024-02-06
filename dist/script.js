acf384cf5f9a7df10cf2e1a6a9ca9bfb6af563ff
Riccardo <64649314+Migl992@users.noreply.github.com>
2024-02-06 14:41:08 +0100

    chain logo and distinction

--- dist/script.js
+++ dist/script.js
@@ -18,38 +18,38 @@ jQuery(document).ready(function(){
 		   throw new Error(`HTTP error! status: ${response.status}`);
 		 }
 		 const data = await response.json();
-		 const chainIdMapping = {
-			"324": "zKSync Era",
-			"1": "Ethereum",
-			"53935": "DFK",
-			"43114": "Avalanche C-Chain",
-			"8453": "Base",
-			"5000": "Mantle",
-			"1992": "Hubble Exchange",
-			"19": "Songbird Canary",
-			"432204": "Dexalot",
-			"10": "Optimism",
-			"14": "Flare Mainnet",
-			"56288": "Boba BNB",
-			"7777777": "Zora",
-			"88888": "Chiliz",
-			"1088": "Metis",
-			"4337": "Beam",
-			"1234": "StepNetwork",
-			"6119": "UPTN",
-			"8888": "XANAchain",
-			"3011": "PLAYA3ULL Games",
-			"333000333": "Meld",
-			"288": "Boba Ethereum",
-			"10507": "Numbers",
-			"7979": "DOS",
-			"73772": "Swimmer",
-			"24052022": "Wraptag",
-			"1228": "XPLUS",
-			"2044": "Shrapnel",
-			"262018": "Loco Legends",
-			"1080": "Mintara",
-			"34443": "Mode"
+		 window.chainIdMapping = {
+			"324": { name: "zKSync Era", imageUrl: "img/zksync.avif" },
+			"1": { name: "Ethereum", imageUrl: "img/ethereum.avif" },
+			"53935": { name: "DFK", imageUrl: "img/DFK.avif" },
+			"43114": { name: "Avalanche C-Chain", imageUrl: "img/avax.avif" },
+			"8453": { name: "Base", imageUrl: "img/base.avif" },
+			"5000": { name: "Mantle", imageUrl: "img/mantle.avif" },
+			"1992": { name: "Hubble Exchange", imageUrl: "img/Hubble.avif" },
+			"19": { name: "Songbird Canary", imageUrl: "img/songbird.avif" },
+			"432204": { name: "Dexalot", imageUrl: "img/DEXALOT.avif" },
+			"10": { name: "Optimism", imageUrl: "img/optimism.avif" },
+			"14": { name: "Flare Mainnet", imageUrl: "img/flare.avif" },
+			"56288": { name: "Boba BNB", imageUrl: "img/boba-bnb.avif" },
+			"7777777": { name: "Zora", imageUrl: "img/zora.avif" },
+			"88888": { name: "Chiliz", imageUrl: "img/chiliz.avif" },
+			"1088": { name: "Metis", imageUrl: "img/metis.avif" },
+			"4337": { name: "Beam", imageUrl: "img/beam.avif" },
+			"1234": { name: "StepNetwork", imageUrl: "img/stepnetwork.avif" },
+			"6119": { name: "UPTN", imageUrl: "img/uptn.avif" },
+			"8888": { name: "XANAchain", imageUrl: "img/xana.avif" },
+			"3011": { name: "PLAYA3ULL Games", imageUrl: "img/Playa3ull.avif" },
+			"333000333": { name: "Meld", imageUrl: "img/meld.avif" },
+			"288": { name: "Boba Ethereum", imageUrl: "img/boba-eth.avif" },
+			"10507": { name: "Numbers", imageUrl: "img/Numbers.avif" },
+			"7979": { name: "DOS", imageUrl: "img/dos.avif" },
+			"73772": { name: "Swimmer", imageUrl: "img/Swimmer.avif" },
+			"24052022": { name: "Wraptag", imageUrl: "img/wraptag.avif" },
+			"1228": { name: "XPLUS", imageUrl: "img/xplus.avif" },
+			"2044": { name: "Shrapnel", imageUrl: "img/Shrapnel.avif" },
+			"262018": { name: "Loco Legends", imageUrl: "img/loco.avif" },
+			"1080": { name: "Mintara", imageUrl: "img/Mintara.avif" },
+			"34443": { name: "Mode", imageUrl: "img/mode.avif" }
 		  };
 		 
 		 // Map the API response to the format expected by FullCalendar
@@ -61,7 +61,7 @@ jQuery(document).ready(function(){
 		   value: item.value,
 		   thxid: item.id,
 		   method: item.methodId,
-		   chain: chainIdMapping[item.chainId] || 'Unknown Chain',
+		   chain: window.chainIdMapping[item.chainId] || 'Unknown Chain',
 		   // Add other event properties as needed
 		 }));
 	   } catch (error) {
@@ -84,10 +84,22 @@ jQuery(document).ready(function(){
 		 },
 		 events: events, // Set the events fetched from the API
 		 eventRender: function(event, element) {
-		   if(event.icon){
-			 element.find(".fc-title").prepend("<i class='fa fa-"+event.icon+"'></i>");
-		   }
-		 },
+			// Check if the chain property is an object and has an imageUrl property
+			const chainInfo = typeof event.chain === 'object' && event.chain !== null ? event.chain : { name: event.chain, imageUrl: '' };
+		  
+			// Only proceed if there's an image URL
+			if (chainInfo.imageUrl) {
+			  // Create an img element with the chain's image URL
+			  var imgElement = document.createElement("img");
+			  imgElement.src = chainInfo.imageUrl;
+			  imgElement.className = "chain-logo"; // Add a class to the image element
+			  imgElement.style.marginRight = "5px"; // Add some space between the image and the title
+		  
+			  // Prepend the img element to the event title
+			  element.find(".fc-title").prepend(imgElement);
+			}
+		  },
+		  
 		 eventClick: function(event) {
 			// Populate the modal with the event details
 			jQuery('.event-title').html(event.title);
@@ -99,7 +111,7 @@ jQuery(document).ready(function(){
 			jQuery('.event-body').append('<p><strong>To:</strong> ' + event.to + '</p>');
 			jQuery('.event-body').append('<p><strong>Value:</strong> ' + event.value + '</p>');
 			jQuery('.event-body').append('<p><strong>Method:</strong> ' + event.method + '</p>');
-			jQuery('.event-body').append('<p><strong>Chain:</strong> ' + event.chain + '</p>');
+			jQuery('.event-body').append('<p><strong>Chain:</strong> ' + event.chain.name + '<img src="' + event.chain.imageUrl + '" /></p>');
 			jQuery('.event-body').append('<p><strong>Link:</strong> <a href="https://routescan.io/tx/' + event.thxid + '" target="_blank">View on RouteScan</a></p>');
 	  
 			// Open the modal
